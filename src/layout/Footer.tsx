328aec008a641930d0295d694f2cdd159799a053
AlvinTuan <letuan7923@gmail.com>
2023-06-19 09:57:55 +0700

    homepage-ver2

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -2,8 +2,28 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <section className="bg-[#090B13] w-screen">
-      <img src="/disney.png" alt="" />
+    <section>
+      <div className="bg-[#090B13] w-screen pt-12 pb-4 text-xs content-center">
+        <img src="/disney.png" alt="" className="mx-auto mb-8" />
+        <div className="flex flex-grow gap-x-5 justify-center mb-9">
+          <span>Política de privacidad</span>
+          <span>Acuerdo de suscripción</span>
+          <span>Ayuda</span>
+          <span>Dispositivos compatible</span>
+          <span>Acerca de Disney+</span>
+          <span>Publicidad personalizada</span>
+        </div>
+        <div className="w-[456px] text-center mx-auto mb-10">
+          <span>
+            Disney+ es un servicio por suscripción de pago, su contenido está
+            sujeto a disponibilidad. El servicio Disney+ es comercializado por
+            Disney DTC LATAM, Inc., 2400 W Alameda AVE., Burbank CA 91521.
+          </span>
+        </div>
+        <div className="w-full text-center mx-auto">
+          <span>© Disney. Todos los derechos reservados.</span>
+        </div>
+      </div>
     </section>
   );
 };
--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,64 +1,68 @@
 import { SearchOutlined } from "@ant-design/icons";
 import { Button, Input, Select, Space } from "antd";
 import { Col, Row } from "antd";
+import { useState } from "react";
 import { NavLink } from "react-router-dom";
 
 const { Search } = Input;
 
 const Header = () => {
   return (
-    <header className="header flex items-center justify-between px-20">
-      <div className="flex items-center justify-center gap-x-14">
-        <img src="/disney.png" alt="" />
-        <div className="flex items-center gap-5">
-          <div className="flex items-center gap-2">
-            <img src="/Inicio.png" alt="" className="object-cover" />
-            <NavLink
-              to="/"
-              className={({ isActive }) => (isActive ? "text-secondary" : "")}
-            >
-              Inicio
-            </NavLink>
-          </div>
-
-          <div className="flex items-center gap-2">
-            <img src="/Originales.png" alt="" className="object-cover" />
-            <NavLink
-              to="originales"
-              className={({ isActive }) => (isActive ? "text-secondary" : "")}
-            >
-              Inicio
-            </NavLink>
-          </div>
+    <header>
+      <div className="grid grid-cols-3 px-5">
+        <img
+          src="/disney.png"
+          alt=""
+          className="flex items-center justify-start"
+        />
+        <div className="flex items-center justify-center">
+          <ul className="p-0 font-bold">
+            <li className="inline-block mx-3">
+              <NavLink
+                to="/"
+                className={({ isActive }) => (isActive ? "text-secondary" : "")}
+              >
+                Inicio
+              </NavLink>
+            </li>
+            <li className="inline-block mx-3">
+              <NavLink
+                to="originales"
+                className={({ isActive }) => (isActive ? "text-secondary" : "")}
+              >
+                Originales
+              </NavLink>
+            </li>
+          </ul>
         </div>
-      </div>
-      <div className="flex items-center gap-x-6">
-        <div className="flex items-center gap-4">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth="1.5"
-            stroke="currentColor"
-            className="w-6 h-6"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+        <div className="flex items-center justify-end">
+          <div className="flex items-center gap-4">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+              />
+            </svg>
+            <input
+              type="text"
+              placeholder="Search"
+              className="bg-inherit w-[200px] h-10"
             />
-          </svg>
-          <input
-            type="text"
-            placeholder="Search"
-            className="bg-inherit w-[200px] h-10"
+          </div>
+          <img
+            src="/Ellipse_6.png"
+            alt=""
+            className="cursor-pointer focus:w-[200px]"
           />
         </div>
-        <img
-          src="/Ellipse_6.png"
-          alt=""
-          className="cursor-pointer focus:w-[200px]"
-        />
       </div>
     </header>
   );
