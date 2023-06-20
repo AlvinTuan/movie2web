34d2ebf42928efda83fb0deaca456004c49420f8
AlvinTuan <letuan7923@gmail.com>
2023-06-20 17:37:10 +0700

    change-swr

--- src/components/movie/MovieList.tsx
+++ src/components/movie/MovieList.tsx
@@ -3,28 +3,34 @@ import MovieCard from "./MovieCard";
 import { SwiperSlide, Swiper } from "swiper/react";
 import { useEffect, useState } from "react";
 import { Movie } from "../../interface";
+import { fetcher } from "../../config";
+import useSWR from "swr";
 
 interface MovieListProps {
   type?: string;
 }
 
+interface MovieType {
+  results: Movie[];
+}
+
 const MovieList: React.FC<MovieListProps> = ({ type = "now_playing" }) => {
-  const [movies, setMovies] = useState<Movie[]>([]);
-  useEffect(() => {
-    const getMovieType = async () => {
-      const res = await axios.get(
-        `https://api.themoviedb.org/3/movie/${type}?api_key=0f2828258a4259490e4802ec8bceef93`
-      );
-      if (res && res.data.results) setMovies(res.data.results);
-    };
-    getMovieType();
-  }, []);
-  // console.log(movies);
+  const { data, error } = useSWR<MovieType>(
+    `https://api.themoviedb.org/3/movie/${type}?api_key=0f2828258a4259490e4802ec8bceef93`,
+    fetcher
+  );
+  if (!data) return null;
+  console.log("🚀 ~ file: MovieList.tsx:36 ~ data:", data);
+
+  const { results } = data;
+  if (!results || results.length <= 0) return null;
+  console.log(results);
+
   return (
     <div className="movie-list">
       <Swiper spaceBetween={40} slidesPerView={"auto"}>
-        {movies.length > 0 &&
-          movies.map((item) => (
+        {results.length > 0 &&
+          results.map((item) => (
             <SwiperSlide key={item.id}>
               <MovieCard item={item}></MovieCard>
             </SwiperSlide>
